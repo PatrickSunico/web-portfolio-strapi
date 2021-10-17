4d14d508109f8ac593557534fe3eb2f90db2c29c
PatrickSunico <apatricksunico@gmail.com>
2021-10-17 18:20:59 +0300

    update

--- src/components/Navbar/NavbarTemplates/NavbarIndex.js
+++ src/components/Navbar/NavbarTemplates/NavbarIndex.js
@@ -1,10 +1,34 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Logo from "../../../assets/icons/Logo.svg"
 import { Link } from "gatsby"
 import * as classes from "../Navbar.module.scss"
-import NavbarLinks from "../NavbarLinks"
+// import NavbarLinks from "../NavbarLinks"
 
-function NavbarIndex({ navbarActive }) {
+import { FaBars } from "react-icons/fa"
+
+import { MenuData } from "../../../data/MenuData"
+
+const NavbarIndex = ({ navbarActive }) => {
+  const [sidebar, setSideBar] = useState(true)
+  const showSideBar = () => setSideBar(!sidebar)
+
+  const [width, setWidth] = useState("")
+
+  const updateWidthAndHeight = () => {
+    setWidth(window.innerWidth)
+  }
+
+  useEffect(() => {
+    // setWidth(window.innerWidth)
+    window.addEventListener("resize", updateWidthAndHeight)
+    if (width < 991 && sidebar) {
+      setSideBar(sidebar)
+    }
+
+    // Cleanup
+    return window.addEventListener("resize", updateWidthAndHeight)
+  }, [width, sidebar])
+
   return (
     <nav
       className={
@@ -14,7 +38,7 @@ function NavbarIndex({ navbarActive }) {
       }
     >
       <div className={`container ${classes["alignment"]}`}>
-        <div className="row">
+        <div className={classes["navbarPosition"]}>
           {/* Logo */}
           <div className={`${classes["logo"]}`}>
             <Link className={classes["logoLink"]} to="/">
@@ -22,9 +46,15 @@ function NavbarIndex({ navbarActive }) {
             </Link>
           </div>
 
+          <FaBars className={`${classes["bar"]}`} onClick={showSideBar} />
+
           {/* Menu */}
           <div className={`${classes["menu"]}`}>
-            <NavbarLinks />
+            {MenuData.map((link, index) => (
+              <Link key={index} to="/" className={`${classes["link"]}`}>
+                {link.title}
+              </Link>
+            ))}
           </div>
         </div>
       </div>
